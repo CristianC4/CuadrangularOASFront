46178c03354b7e8747032fb80a60b469d6a2e8b0
CristianC4 <31807256+CristianC4@users.noreply.github.com>
2021-05-27 17:00:09 -0500

    Delete src/app directory

--- src/app/DTO/teamDTO.ts
+++
@@ -1,6 +0,0 @@
-export class teamDTO {
-    name_team: String = "";
-    date_team: String = "";
-    country_team: String = "";
-    city_team: String = "";
-}
--- src/app/DTO/tournamentDTO.ts
+++
@@ -1,5 +0,0 @@
-export class tournamentDTO{
-    name_tournament: String = "";
-    date_start_tournament: String = '2021-05-27';
-    date_end_tournament: String = "2021-05-27";
-}
